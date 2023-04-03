a0ad1d923cc7957fdfa796bef104b46d6ac71bbb
Stephen <71991128+stephen601@users.noreply.github.com>
2023-04-02 19:42:32 -0600

    Updated functions

--- Part 3/proj.py
+++ Part 3/proj.py
@@ -111,9 +111,73 @@ def query4(conn):
 
     except IndexError as e:
         print("You need to supply a parameter.")
-
-
-
+def query5(conn):
+     try:
+        cursor = conn.cursor()
+        cursor.execute("""
+            SELECT Administrator.empId, Administrator.name, SUM(AdmWorkHours.hours) AS total_working_hours
+            FROM Administrator
+            JOIN AdmWorkHours ON Administrator.empId = AdmWorkHours.empId
+            GROUP BY Administrator.empId
+            ORDER BY total_working_hours ASC
+            """)
+        rows = cursor.fetchall()
+        for row in rows:
+            print(row[0], row[1], row[2])
+     except Error as e:
+        print(e)    
+def query6(conn):
+    try:
+        cursor = conn.cursor()
+        model_no = sys.argv[2]
+        cursor.execute("""
+            SELECT TechnicalSupport.name
+            FROM TechnicalSupport
+            JOIN Specializes ON TechnicalSupport.empId = Specializes.empId
+            WHERE Specializes.modelNo = ?
+            """, (model_no,))
+        rows = cursor.fetchall()
+        for row in rows:
+            print(row[0])
+    except Error as e:
+       print(e)
+def query7(conn):
+    try:
+        cursor = conn.cursor()
+        cursor.execute("""
+            SELECT Salesman.name, AVG(Purchases.commissionRate) AS avg_commission_rate
+            FROM Salesman
+            JOIN Purchases ON Salesman.empId = Purchases.empId
+            GROUP BY Salesman.empId
+            ORDER BY avg_commission_rate DESC
+            """)
+        rows = cursor.fetchall()
+        max_len_name = max(len(row[0]) for row in rows)
+        max_len_rate = max(len(str(row[1])) for row in rows)
+        print(f"{'Name'.ljust(max_len_name)} {'Average Commission Rate'}")
+        print("------------------".ljust(max_len_name + max_len_rate + 1, "-"))
+        for row in rows:
+            print(f"{row[0].ljust(max_len_name)} {row[1]:.2f}")
+    except Error as e:
+       print(e)
+def query8(conn):
+    try:
+        cursor = conn.cursor()
+        cursor.execute("""
+            SELECT 'Administrator' AS Role, COUNT(*) AS cnt FROM Administrator
+            UNION
+            SELECT 'Salesmen' AS Role, COUNT(*) AS cnt FROM Salesman
+            UNION
+            SELECT 'Technicians' AS Role, COUNT(*) AS cnt FROM TechnicalSupport
+            """)
+        rows = cursor.fetchall()
+        max_len = max(len(row[0]) for row in rows)
+        print(f"{'Role'.ljust(max_len)} {'cnt'}")
+        print("------------------")
+        for row in rows:
+            print(f"{row[0].ljust(max_len)} {row[1]}")
+    except Error as e:
+       print(e)
 
 
 def main():
@@ -133,74 +197,14 @@ def main():
             query3(conn)
         elif queryNumber == 4:
             query4(conn)
-         elif queryNumber==5:
-            try:
-                cursor = conn.cursor()
-                cursor.execute("""
-                    SELECT Administrator.empId, Administrator.name, SUM(AdmWorkHours.hours) AS total_working_hours
-                    FROM Administrator
-                    JOIN AdmWorkHours ON Administrator.empId = AdmWorkHours.empId
-                    GROUP BY Administrator.empId
-                    ORDER BY total_working_hours ASC
-                """)
-                rows = cursor.fetchall()
-                for row in rows:
-                    print(row[0], row[1], row[2])
-            except Error as e:
-                print(e)
+        elif queryNumber == 5:
+            query5(conn)
         elif queryNumber==6:
-            try:
-                cursor = conn.cursor()
-                model_no = sys.argv[2]
-                cursor.execute("""
-                    SELECT TechnicalSupport.name
-                    FROM TechnicalSupport
-                    JOIN Specializes ON TechnicalSupport.empId = Specializes.empId
-                    WHERE Specializes.modelNo = ?
-                """, (model_no,))
-                rows = cursor.fetchall()
-                for row in rows:
-                    print(row[0])
-            except Error as e:
-                print(e)
+            query6(conn)
         elif queryNumber==7:
-            try:
-                cursor = conn.cursor()
-                cursor.execute("""
-                    SELECT Salesman.name, AVG(Purchases.commissionRate) AS avg_commission_rate
-                    FROM Salesman
-                    JOIN Purchases ON Salesman.empId = Purchases.empId
-                    GROUP BY Salesman.empId
-                    ORDER BY avg_commission_rate DESC
-                """)
-                rows = cursor.fetchall()
-                max_len_name = max(len(row[0]) for row in rows)
-                max_len_rate = max(len(str(row[1])) for row in rows)
-                print(f"{'Name'.ljust(max_len_name)} {'Average Commission Rate'}")
-                print("------------------".ljust(max_len_name + max_len_rate + 1, "-"))
-                for row in rows:
-                        print(f"{row[0].ljust(max_len_name)} {row[1]:.2f}")
-            except Error as e:
-                print(e)
+            query7(conn)
         elif queryNumber==8:
-            try:
-                cursor = conn.cursor()
-                cursor.execute("""
-                    SELECT 'Administrator' AS Role, COUNT(*) AS cnt FROM Administrator
-                    UNION
-                    SELECT 'Salesmen' AS Role, COUNT(*) AS cnt FROM Salesman
-                    UNION
-                    SELECT 'Technicians' AS Role, COUNT(*) AS cnt FROM TechnicalSupport
-                """)
-                rows = cursor.fetchall()
-                max_len = max(len(row[0]) for row in rows)
-                print(f"{'Role'.ljust(max_len)} {'cnt'}")
-                print("------------------")
-                for row in rows:
-                        print(f"{row[0].ljust(max_len)} {row[1]}")
-            except Error as e:
-                print(e)
-
+            query8(conn)
         close_connection(conn)
 
     else:
